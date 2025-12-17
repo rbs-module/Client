05e82aca3064523b0bfe645123753c7b42bc2c73
Rakib <rakibulhasan.bdm@gmail.com>
2025-12-17 19:22:20 +0600

    update create expense

--- src/constant/base-url.ts
+++ src/constant/base-url.ts
@@ -1,4 +1,5 @@
 // export const BASE_URL = "http://localhost:4000";
 // export const BASE_URL = "";
-export const BASE_URL = "https://api.embroart.com.bd";
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL ?? "https://api.embroart.com.bd";
 // export const BASE_URL = "http://192.168.1.108:4000";
